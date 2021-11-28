cfd90d4ddde588a0cd102236d14dae9d1359ae60
echofrost38 <monkey199294@gmail.com>
2021-11-28 13:08:42 +0200

    rename LinkedIn

---
+++ src/main/java/com/ndb/auction/security/oauth2/user/LinkedInOAuth2UserInfo.java
@@ -0,0 +1,35 @@
+package com.ndb.auction.security.oauth2.user;
+
+import java.util.Map;
+public class LinkedInOAuth2UserInfo extends OAuth2UserInfo {
+
+	public LinkedInOAuth2UserInfo(Map<String, Object> attributes) {
+		super(attributes);
+	}
+
+	@Override
+	public String getId() {
+		return (String) attributes.get("id");
+	}
+
+	@Override
+	public String getName() {
+		return ((String) attributes.get("localizedFirstName")).concat(" ").concat((String) attributes.get("localizedLastName"));
+	}
+
+	@Override
+	public String getEmail() {
+		return (String) attributes.get("emailAddress");
+	}
+
+	@Override
+	public String getImageUrl() {
+		return (String) attributes.get("pictureUrl");
+	}
+
+    @Override
+    public String getLocale() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}

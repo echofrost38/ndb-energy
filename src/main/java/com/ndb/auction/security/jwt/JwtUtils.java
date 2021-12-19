7b9e3665e0f03ab1d688e20624dcff6d861f31b9
echofrost38 <monkey199294@gmail.com>
2021-12-19 08:27:28 +0000

    fix: Remove Environments

--- src/main/java/com/ndb/auction/security/jwt/JwtUtils.java
+++ src/main/java/com/ndb/auction/security/jwt/JwtUtils.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +13,8 @@ import com.ndb.auction.service.UserDetailsImpl;
 import io.jsonwebtoken.*;
 
 @Component
-public class JwtUtils implements EnvironmentAware {
+public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
-
-	@Override
-	public void setEnvironment(Environment environment) {
-		this.jwtSecret = environment.getProperty("app.jwt.secret");
-		this.jwtExpirationMs = Integer.valueOf(environment.getProperty("app.jwt.expire"));
-	}
 
 	@Value("${app.jwtSecret}")
 	private String jwtSecret;

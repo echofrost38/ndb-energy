d9602132cae26131f1abe7e2998788a44d74883e
echofrost38 <monkey199294@gmail.com>
2022-02-09 16:33:24 +0000

    Update ShuftiDao.java

--- src/main/java/com/ndb/auction/dao/oracle/ShuftiDao.java
+++ src/main/java/com/ndb/auction/dao/oracle/ShuftiDao.java
@@ -53,7 +53,7 @@ public class ShuftiDao extends BaseOracleDao {
 
     public int insert(ShuftiReference m) {
         String sql = "INSERT INTO TBL_SHUFTI_REF(USER_ID, REFERENCE, VERIFY_TYPE, DOC_STATUS, ADDR_STATUS, CON_STATUS)"
-				+ "VALUES(?,?,?)";
+				+ "VALUES(?,?,?,?,?,?)";
 		return jdbcTemplate.update(sql, m.getUserId(), m.getReference(), m.getVerificationType(), m.getDocStatus(), m.getAddrStatus(), m.getConStatus());
     }
 

adc92839cdec1add52fd76896c5201bc759c2356
echofrost38 <monkey199294@gmail.com>
2022-03-11 00:12:30 +0300

    New table named TBL_STRIPE_DEPOSIT has been created and service for that table has been added.

---
+++ src/main/java/com/ndb/auction/dao/oracle/transactions/stripe/StripeDepositDao.java
@@ -0,0 +1,114 @@
+package com.ndb.auction.dao.oracle.transactions.stripe;
+
+import com.ndb.auction.dao.oracle.BaseOracleDao;
+import com.ndb.auction.dao.oracle.Table;
+import com.ndb.auction.dao.oracle.transactions.ITransactionDao;
+import com.ndb.auction.models.transactions.Transaction;
+import com.ndb.auction.models.transactions.stripe.StripeDepositTransaction;
+import lombok.NoArgsConstructor;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+@NoArgsConstructor
+@Table(name = "TBL_STRIPE_DEPOSIT")
+public class StripeDepositDao extends BaseOracleDao implements ITransactionDao, IStripeDao {
+
+    private static StripeDepositTransaction extract(ResultSet rs) throws SQLException {
+        StripeDepositTransaction m = new StripeDepositTransaction();
+        m.setId(rs.getInt("ID"));
+        m.setUserId(rs.getInt("USER_ID"));
+        m.setAmount(rs.getLong("AMOUNT"));
+        m.setCreatedAt(rs.getTimestamp("CREATED_AT").getTime());
+        m.setConfirmedAt(rs.getTimestamp("UPDATED_AT").getTime());
+        m.setStatus(rs.getBoolean("STATUS"));
+        m.setFiatType(rs.getString("FIAT_TYPE"));
+        m.setFiatAmount(rs.getLong("FIAT_AMOUNT"));
+        m.setPaymentIntentId(rs.getString("INTENT_ID"));
+        m.setPaymentMethodId(rs.getString("METHOD_ID"));
+        m.setCryptoType(rs.getString("CRYPTO_TYPE"));
+        m.setCryptoPrice(rs.getDouble("CRYPTO_PRICE"));
+        m.setFee(rs.getDouble("FEE"));
+        m.setDeposited(rs.getDouble("DEPOSITED"));
+        return m;
+    }
+
+    @Override
+    public StripeDepositTransaction selectByStripeIntentId(String intentId) {
+        String sql = "SELECT * FROM TBL_STRIPE_DEPOSIT WHERE INTENT_ID=?";
+        return jdbcTemplate.query(sql, rs -> {
+            if (!rs.next())
+                return null;
+            return extract(rs);
+        }, intentId);
+    }
+
+    @Override
+    public Transaction insert(Transaction _m) {
+        StripeDepositTransaction m = (StripeDepositTransaction) _m;
+        String sql = "INSERT INTO TBL_STRIPE_DEPOSIT(ID,USER_ID,AMOUNT,CREATED_AT,UPDATED_AT,STATUS,FIAT_TYPE,FIAT_AMOUNT,INTENT_ID,METHOD_ID,CRYPTO_TYPE,CRYPTO_PRICE,FEE,DEPOSITED)"
+                + " VALUES(SEQ_STRIPE_DEPOSIT.NEXTVAL,?,?,SYSDATE,SYSDATE,0,?,?,?,?,?,?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    @Override
+                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                        PreparedStatement ps = connection.prepareStatement(sql,
+                                new String[] { "ID" });
+                        int i = 1;
+                        ps.setInt(i++, m.getUserId());
+                        ps.setDouble(i++, m.getAmount());
+                        ps.setString(i++, m.getFiatType());
+                        ps.setDouble(i++, m.getFiatAmount());
+                        ps.setString(i++, m.getPaymentIntentId());
+                        ps.setString(i++, m.getPaymentMethodId());
+                        ps.setString(i++, m.getCryptoType());
+                        ps.setDouble(i++, m.getCryptoPrice());
+                        ps.setDouble(i++, m.getFee());
+                        ps.setDouble(i++, m.getDeposited());
+                        return ps;
+                    }
+                }, keyHolder);
+        m.setId(keyHolder.getKey().intValue());
+        return m;    }
+
+    @Override
+    public List<? extends Transaction> selectAll(String orderBy) {
+        String sql = "SELECT * FROM TBL_STRIPE_DEPOSIT";
+        if (orderBy == null)
+            orderBy = "ID";
+        sql += " ORDER BY " + orderBy;
+        return jdbcTemplate.query(sql, (rs, rownumber) -> extract(rs));    }
+
+    @Override
+    public List<? extends Transaction> selectByUser(int userId, String orderBy) {
+        String sql = "SELECT * FROM TBL_STRIPE_DEPOSIT WHERE USER_ID = ?";
+        if (orderBy == null)
+            orderBy = "ID";
+        sql += " ORDER BY " + orderBy;
+        return jdbcTemplate.query(sql, (rs, rownumber) -> extract(rs), userId);    }
+
+    @Override
+    public Transaction selectById(int id) {
+        String sql = "SELECT * FROM TBL_STRIPE_DEPOSIT WHERE ID=?";
+        return jdbcTemplate.query(sql, rs -> {
+            if (!rs.next())
+                return null;
+            return extract(rs);
+        }, id);    }
+
+    @Override
+    public int update(int id, int status) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/ndb/auction/service/payment/stripe/StripeDepositService.java
@@ -0,0 +1,159 @@
+package com.ndb.auction.service.payment.stripe;
+
+import com.ndb.auction.dao.oracle.transactions.stripe.StripeDepositDao;
+import com.ndb.auction.models.Notification;
+import com.ndb.auction.models.transactions.Transaction;
+import com.ndb.auction.models.transactions.stripe.StripeCustomer;
+import com.ndb.auction.models.transactions.stripe.StripeDepositTransaction;
+import com.ndb.auction.payload.response.PayResponse;
+import com.ndb.auction.service.payment.ITransactionService;
+import com.stripe.model.Customer;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.PaymentMethod;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StripeDepositService extends StripeBaseService implements ITransactionService {
+
+    private final StripeDepositDao stripeDepositDao;
+
+    @Autowired
+    public StripeDepositService(StripeDepositDao stripeDepositDao) {
+        this.stripeDepositDao = stripeDepositDao;
+    }
+
+    public PayResponse createDeposit(StripeDepositTransaction m, boolean isSaveCard) {
+        int userId = m.getUserId();
+        PaymentIntent intent = null;
+        PayResponse response = new PayResponse();
+        try {
+            if(m.getPaymentIntentId() == null) {
+                PaymentIntentCreateParams.Builder createParams = PaymentIntentCreateParams.builder()
+                        .setAmount(m.getAmount())
+                        .setCurrency("USD")
+                        .setConfirm(true)
+                        .setPaymentMethod(m.getPaymentMethodId())
+                        .setConfirmationMethod(PaymentIntentCreateParams.ConfirmationMethod.MANUAL);
+
+                if(isSaveCard) {
+                    var customer = Customer.create(new CustomerCreateParams.Builder().setPaymentMethod(m.getPaymentMethodId()).build());
+                    createParams.setCustomer(customer.getId());
+                    createParams.setSetupFutureUsage(PaymentIntentCreateParams.SetupFutureUsage.OFF_SESSION);
+
+                    // save customer
+                    var method = PaymentMethod.retrieve(m.getPaymentMethodId());
+
+                    var card = method.getCard();
+                    var stripeCustomer = new StripeCustomer(
+                            m.getUserId(), customer.getId(), card.getBrand(), card.getCountry(), card.getExpMonth(), card.getExpYear(), card.getLast4()
+                    );
+
+                    stripeCustomerDao.insert(stripeCustomer);
+                }
+
+                intent = PaymentIntent.create(createParams.build());
+            } else if (m.getPaymentIntentId() != null) {
+                intent = PaymentIntent.retrieve(m.getPaymentIntentId());
+                intent = intent.confirm();
+                m = (StripeDepositTransaction) stripeDepositDao.insert(m);
+            }
+
+            if(intent != null && intent.getStatus().equals("succeeded")) {
+
+                // get real payment!
+                handleDepositSuccess(userId, intent, m.getCryptoType());
+            }
+            response = generateResponse(intent, response);
+        } catch (Exception e) {
+            response.setError(e.getMessage());
+        }
+        return response;
+    }
+
+    public PayResponse createDepositWithSavedCard(StripeDepositTransaction m, String customerId) {
+        int userId = m.getUserId();
+        PaymentIntent intent = null;
+        PayResponse response = new PayResponse();
+        try {
+            if(m.getPaymentIntentId() == null) {
+                PaymentIntentCreateParams.Builder createParams = PaymentIntentCreateParams.builder()
+                        .setAmount(m.getAmount())
+                        .setCurrency("USD")
+                        .setCustomer(customerId)
+                        .setConfirm(true)
+                        .setConfirmationMethod(PaymentIntentCreateParams.ConfirmationMethod.MANUAL);
+
+                intent = PaymentIntent.create(createParams.build());
+            } else if (m.getPaymentIntentId() != null) {
+                intent = PaymentIntent.retrieve(m.getPaymentIntentId());
+                intent = intent.confirm();
+                m = (StripeDepositTransaction) stripeDepositDao.insert(m);
+            }
+
+            if(intent != null && intent.getStatus().equals("succeeded")) {
+                handleDepositSuccess(userId, intent, m.getCryptoType());
+            }
+            response = generateResponse(intent, response);
+        } catch (Exception e) {
+            response.setError(e.getMessage());
+        }
+        return response;
+    }
+
+    private void handleDepositSuccess(int userId, PaymentIntent intent, String cryptoType) {
+
+        long amount = intent.getAmount();
+        double fee = getStripeFee(amount);
+        double cryptoPrice = 1.0;
+        if(!cryptoType.equals("USDT")) {
+            cryptoPrice = thirdAPIUtils.getCryptoPriceBySymbol(cryptoType);
+        }
+
+        double deposited = (amount - fee) / cryptoPrice;
+        var m = new StripeDepositTransaction(
+                userId,amount,cryptoType,cryptoPrice, intent.getId(),
+                intent.getPaymentMethod(),fee,deposited);
+
+        insert(m);
+
+        notificationService.sendNotification(
+                userId,
+                Notification.DEPOSIT_SUCCESS,
+                "Deposit Successful",
+                String.format("You have successfully deposited %f %s", Double.valueOf(amount) / 100, cryptoType)
+        );
+    }
+
+    public StripeDepositTransaction insert(StripeDepositTransaction m) {
+        return (StripeDepositTransaction) stripeDepositDao.insert(m);
+    }
+
+    @Override
+    public List<? extends Transaction> selectAll(String orderBy) {
+        return stripeDepositDao.selectAll(orderBy);
+    }
+
+    @Override
+    public List<? extends Transaction> selectByUser(int userId, String orderBy) {
+        return stripeDepositDao.selectByUser(userId, orderBy);
+    }
+
+    @Override
+    public Transaction selectById(int id) {
+        return stripeDepositDao.selectById(id);
+    }
+
+    @Override
+    public int update(int id, int status) {
+        return 0;
+    }
+
+    public StripeDepositTransaction selectByIntentId(String intentId) {
+        return stripeDepositDao.selectByStripeIntentId(intentId);
+    }
+}

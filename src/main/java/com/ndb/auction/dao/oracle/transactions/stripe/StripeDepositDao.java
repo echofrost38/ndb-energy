6452e23295ef2dd97edd45285d3b7a92e634f4f2
echofrost38 <monkey199294@gmail.com>
2022-03-16 23:38:41 +0300

    StripeDepositDao edited.

--- src/main/java/com/ndb/auction/dao/oracle/transactions/stripe/StripeDepositDao.java
+++ src/main/java/com/ndb/auction/dao/oracle/transactions/stripe/StripeDepositDao.java
@@ -6,12 +6,10 @@ import com.ndb.auction.dao.oracle.transactions.ITransactionDao;
 import com.ndb.auction.models.transactions.Transaction;
 import com.ndb.auction.models.transactions.stripe.StripeDepositTransaction;
 import lombok.NoArgsConstructor;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -58,24 +56,21 @@ public class StripeDepositDao extends BaseOracleDao implements ITransactionDao, 
                 + " VALUES(SEQ_STRIPE_DEPOSIT.NEXTVAL,?,?,SYSDATE,SYSDATE,0,?,?,?,?,?,?,?,?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(
-                new PreparedStatementCreator() {
-                    @Override
-                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                        PreparedStatement ps = connection.prepareStatement(sql,
-                                new String[] { "ID" });
-                        int i = 1;
-                        ps.setInt(i++, m.getUserId());
-                        ps.setDouble(i++, m.getAmount());
-                        ps.setString(i++, m.getFiatType());
-                        ps.setDouble(i++, m.getFiatAmount());
-                        ps.setString(i++, m.getPaymentIntentId());
-                        ps.setString(i++, m.getPaymentMethodId());
-                        ps.setString(i++, m.getCryptoType());
-                        ps.setDouble(i++, m.getCryptoPrice());
-                        ps.setDouble(i++, m.getFee());
-                        ps.setDouble(i++, m.getDeposited());
-                        return ps;
-                    }
+                connection -> {
+                    PreparedStatement ps = connection.prepareStatement(sql,
+                            new String[] { "ID" });
+                    int i = 1;
+                    ps.setInt(i++, m.getUserId());
+                    ps.setDouble(i++, m.getAmount());
+                    ps.setString(i++, m.getFiatType());
+                    ps.setDouble(i++, m.getFiatAmount());
+                    ps.setString(i++, m.getPaymentIntentId());
+                    ps.setString(i++, m.getPaymentMethodId());
+                    ps.setString(i++, m.getCryptoType());
+                    ps.setDouble(i++, m.getCryptoPrice());
+                    ps.setDouble(i++, m.getFee());
+                    ps.setDouble(i, m.getDeposited());
+                    return ps;
                 }, keyHolder);
         m.setId(keyHolder.getKey().intValue());
         return m;    }

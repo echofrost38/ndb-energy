1649f99dea1f564865c26a2b5de05df33306133b
echofrost38 <monkey199294@gmail.com>
2022-03-22 22:42:09 +0300

    ShuftiResponse has been edited to receive other fields.

--- src/main/java/com/ndb/auction/models/Shufti/Response/AdditionalData.java
+++ src/main/java/com/ndb/auction/models/Shufti/Response/AdditionalData.java
@@ -2,10 +2,10 @@ package com.ndb.auction.models.Shufti.Response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-
-import lombok.*;
-
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -13,12 +13,10 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Document {
-    private String document_number;
-    private Name name;
-    private String dob;
-    private int age;
-    private String gender;
-    private String[] selected_type;
-    private String[] supported_types;
+public class AdditionalData {
+    private Doc document;
+}
+
+class Doc {
+    private Proof proof;
 }

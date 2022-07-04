f73e835f42894c5b882176866200fb6464371f6e
echofrost38 <monkey199294@gmail.com>
2022-07-05 00:32:20 +0300

    add: log

--- src/main/java/com/ndb/auction/utils/RemoteIpHelper.java
+++ src/main/java/com/ndb/auction/utils/RemoteIpHelper.java
@@ -1,7 +1,14 @@
 package com.ndb.auction.utils;
 
 import static com.ndb.auction.utils.HttpHeader.*;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,7 +17,7 @@ public class RemoteIpHelper {
 
     private static final String UNKNOWN = "unknown";
 
-    public static String getRemoteIpFrom(HttpServletRequest request) {
+    public static String getRemoteIpFrom(HttpServletRequest request) throws IOException {
         String ip = null;
         int tryCount = 1;
 
@@ -38,12 +45,27 @@ public class RemoteIpHelper {
         }
         // check ip contains comma
         log.info("origin ip: {}", ip);
-        log.info("origin count: {}", tryCount);
+        log.info("url: {}", getUrl(request));
+        log.info("data: {}",
+                IOUtils.toString(new BufferedInputStream(request.getInputStream()), StandardCharsets.UTF_8));
+
         var ipArr = ip.split(",");
         return ipArr[0];
     }
 
     private static boolean isIpFound(String ip) {
         return ip != null && ip.length() > 0 && !UNKNOWN.equalsIgnoreCase(ip);
+    }
+
+    private static String getUrl(HttpServletRequest req) {
+        String scheme = req.getScheme();
+        String serverName = req.getServerName();
+        int serverPort = req.getServerPort();
+        String uri = req.getRequestURI();
+        String prmstr = req.getQueryString();
+        // String uri = (String) req.getAttribute("javax.servlet.forward.request_uri");
+        // String prmstr = (String)
+        // req.getAttribute("javax.servlet.forward.query_string");
+        return scheme + "://" + serverName + ":" + serverPort + uri + "?" + prmstr;
     }
 }

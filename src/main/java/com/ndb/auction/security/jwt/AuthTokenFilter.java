ecef080da7632c2c752c8ed2b772010332f5db9f
echofrost38 <monkey199294@gmail.com>
2022-07-04 22:42:51 +0300

    add whitelist: nyyupay, ndbcoin

--- src/main/java/com/ndb/auction/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/ndb/auction/security/jwt/AuthTokenFilter.java
@@ -142,6 +142,8 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 matcher.match("/shufti", request.getServletPath()) ||
                 matcher.match("/stripe", request.getServletPath()) ||
                 matcher.match("/ipn/**", request.getServletPath()) ||
+                matcher.match("/nyyupay/**", request.getServletPath()) ||
+                matcher.match("/ndbcoin/**", request.getServletPath()) ||
                 matcher.match("/paypal/**", request.getServletPath());
     }
 

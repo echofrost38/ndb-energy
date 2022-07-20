f43dbb03d8015bdbf5b58a5e058a75e7818d9615
echofrost38 <monkey199294@gmail.com>
2022-07-20 12:37:03 +0300

    add: presale 30 days

--- src/main/java/com/ndb/auction/hooks/NDBcoinController.java
+++ src/main/java/com/ndb/auction/hooks/NDBcoinController.java
@@ -7,13 +7,11 @@ import com.ndb.auction.models.TotalSupply;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.text.DecimalFormat;
 
 @RestController
 public class NDBcoinController extends BaseController {
     @Autowired
     P2pController p2pController;
-    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     @RequestMapping(value = "/totalsupply", method = RequestMethod.GET)
     public TotalSupply totalSupply() throws Exception {
@@ -27,7 +25,6 @@ public class NDBcoinController extends BaseController {
         return new CirculatingSupply(circulatingSupply);
     }
 
-    @SuppressWarnings("unchecked")
     @RequestMapping(value = "/marketcap", method = RequestMethod.GET)
     public Marketcap marketcap() throws Exception {
         double marketcap = ndbCoinService.getMarketCap();

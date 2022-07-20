8d7f647a4b3d057b8104d9c660a5fc29a75e8e39
echofrost38 <monkey199294@gmail.com>
2022-07-20 20:32:16 +0300

    fix: crypto price

--- src/main/java/com/ndb/auction/hooks/CryptoController.java
+++ src/main/java/com/ndb/auction/hooks/CryptoController.java
@@ -264,7 +264,7 @@ public class CryptoController extends BaseController {
                     Notification.DEPOSIT_SUCCESS, 
                     "PAYMENT CONFIRMED", 
                     "Your purchase of " + ndbToken + "NDB" + " in the presale round was successful.");
-                var price = apiUtil.getCryptoPriceBySymbol("USDT");
+                var price = apiUtil.getCryptoPriceBySymbol(cryptoType);
                 log.info("added free balance: {}", fiatAmount / price);
                 balanceService.addFreeBalance(presaleOrder.getUserId(), cryptoType, fiatAmount / price);
                 return new ResponseEntity<>(HttpStatus.OK); 

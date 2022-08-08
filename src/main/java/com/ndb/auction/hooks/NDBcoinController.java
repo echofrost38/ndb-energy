c100d4ef7671fecb3ff1a26a3fec7b7889a0d853
echofrost38 <monkey199294@gmail.com>
2022-08-09 00:02:47 +0900

    added rest api to get ndbcoin price and kline data from p2pb2b and digifinex services

--- src/main/java/com/ndb/auction/hooks/NDBcoinController.java
+++ src/main/java/com/ndb/auction/hooks/NDBcoinController.java
@@ -1,14 +1,26 @@
 package com.ndb.auction.hooks;
 
+import com.google.gson.Gson;
 import com.ndb.auction.controllers.P2pController;
 import com.ndb.auction.models.CirculatingSupply;
 import com.ndb.auction.models.Marketcap;
 import com.ndb.auction.models.TotalSupply;
 
+import com.ndb.auction.models.digifinex.DigiFinex;
+import com.ndb.auction.models.p2pb2b.P2PB2BResponse;
+import com.ndb.auction.web3.NDBCoinService;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class NDBcoinController extends BaseController {
@@ -33,4 +45,5 @@ public class NDBcoinController extends BaseController {
         double marketcap = ndbCoinService.getMarketCap();
         return new Marketcap(df.format(marketcap));
     }
+
 }

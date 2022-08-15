b3a45ee7778adc32c4c79faa2d1d1051214b3adb
echofrost38 <monkey199294@gmail.com>
2022-08-15 18:04:51 +0800

    warning fix

--- src/main/java/com/ndb/auction/controllers/ServerManageController.java
+++ src/main/java/com/ndb/auction/controllers/ServerManageController.java
@@ -4,8 +4,6 @@ import com.ndb.auction.config.AppConfig;
 import com.ndb.auction.hooks.BaseController;
 import com.ndb.auction.models.ServerMaintenance;
 import com.ndb.auction.service.ServerManageService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

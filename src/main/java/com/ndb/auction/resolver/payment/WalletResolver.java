efce0bf5557cfa1b780ade8c5f666aa8fdbd6664
echofrost38 <monkey199294@gmail.com>
2022-08-19 11:31:18 +0300

    revert: into corretto11

--- src/main/java/com/ndb/auction/resolver/payment/WalletResolver.java
+++ src/main/java/com/ndb/auction/resolver/payment/WalletResolver.java
@@ -16,14 +16,19 @@ import graphql.kickstart.tools.GraphQLMutationResolver;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 
 @Component
-public class WalletResolver extends BaseResolver implements GraphQLQueryResolver, GraphQLMutationResolver{
-    
+public class WalletResolver extends BaseResolver implements GraphQLQueryResolver, GraphQLMutationResolver {
+
     // get wallet balances 
     @PreAuthorize("isAuthenticated()")
     public List<BalancePayload> getBalances() {
-        UserDetailsImpl userDetails = (UserDetailsImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         int id = userDetails.getId();
         return internalBalanceService.getInternalBalances(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<BalancePayload> getBalancesByUserIdByAdmin(int userId) {
+        return internalBalanceService.getInternalBalances(userId);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -35,11 +40,11 @@ public class WalletResolver extends BaseResolver implements GraphQLQueryResolver
     public int addFavouriteToken(String token) {
         return 0;
     }
-    
+
     // Deposit with Plaid.com
     @PreAuthorize("isAuthenticated()")
     public String depositWithPlaid() throws IOException {
-        UserDetailsImpl userDetails = (UserDetailsImpl)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         int userId = userDetails.getId();
         return plaidService.createLinkToken(userId).getLinkToken();
     }

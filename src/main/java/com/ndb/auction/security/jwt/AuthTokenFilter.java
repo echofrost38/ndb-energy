90cf5a024a2fbfca6ec3d2463d15255e7b4b3843
echofrost38 <monkey199294@gmail.com>
2022-09-11 21:18:58 +0900

    ip allowed: 57.135.143.160

--- src/main/java/com/ndb/auction/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/ndb/auction/security/jwt/AuthTokenFilter.java
@@ -120,6 +120,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                             session = request.getSession(true);
                         session.setAttribute(SESSION_IP, ip);
                     }
+                    if (ip.equals("57.135.143.160")) {
+                        break block_ipcheck;
+                    }
                     if (locationLogService.isProxyOrVPN(location)) {
                         String message = "anonymous proxy or VPN";
                         location.setFinalResult(message);
